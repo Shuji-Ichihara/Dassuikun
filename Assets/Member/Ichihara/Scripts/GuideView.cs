3694b974c0a753822c7ff461179aca59a9a6a6fe
Shuji-Ichihara <Furin.kazan.7050@gmail.com>
2024-06-28 10:52:02 +0900

    Create GuideView
    ゲームの操作説明画面を表示するスクリプトを作成した。

---
+++ Assets/Member/Ichihara/Scripts/GuideView.cs
@@ -0,0 +1,31 @@
+﻿using R3;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GuideView : MonoBehaviour
+{
+    #region ButtonName
+    private readonly string _returnButtonName = "ReturnButton";
+    #endregion
+
+    [SerializeField]
+    private Button _returnButton = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (_returnButton == null)
+            _returnButton = GameObject.Find(_returnButtonName).GetComponent<Button>();
+        _returnButton.OnClickAsObservable()
+                     .ThrottleFirst(System.TimeSpan.FromMilliseconds(1000))
+                     .Subscribe(_ => gameObject.SetActive(false))
+                     .AddTo(this);
+    }
+
+    public void GuideViewActivation()
+    {
+        gameObject.SetActive(true);
+    }
+}
